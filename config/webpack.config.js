e63765e9ba3b93fc0965f1f4f144d99f95673557
Vuzeta <34634478+Vuzeta@users.noreply.github.com>
2020-01-19 14:08:04 +0100

    fixed svg view

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,62 +5,57 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	mode: 'development',
-	entry: './src/js/main.js',
-	output: {
-		path: path.resolve(__dirname, '../', 'dist'),
-		filename: '[name].js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(sass|scss)$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /\.svg/,
-				use: {
-					loader: 'svg-url-loader',
-					options: {},
-				},
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							outputPath: 'assets',
-							publicPath: 'assets',
-						},
-					},
-					{
-						loader: 'image-webpack-loader',
-					},
-				],
-			},
-			{
-				test: /\.js$/,
-				loader: ['babel-loader'],
-				exclude: /node_modules/,
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'src/index.html',
-		}),
-		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin({
-			filename: '[name].css',
-			publicPath: '../',
-		}),
-		// new CopyPlugin([
-		// 	{
-		// 		from: 'src/assets',
-		// 		to: 'assets',
-		// 	},
-		// ]),
-	],
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, '../', 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets',
+              publicPath: 'assets',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {},
+        },
+      },
+      {
+        test: /\.js$/,
+        loader: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      publicPath: '../',
+    }),
+    new CopyPlugin([{ from: 'src/assets', to: 'assets' }]),
+  ],
 };
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -2,57 +2,60 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	mode: 'production',
-	entry: './src/js/main.js',
-	output: {
-		path: path.resolve(__dirname, '../', 'dist'),
-		filename: '[name]-[contenthash:10].js',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(sass|scss)$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							outputPath: 'assets',
-						},
-					},
-					{
-						loader: 'image-webpack-loader',
-					},
-				],
-			},
-			{
-				test: /\.svg/,
-				use: {
-					loader: 'svg-url-loader',
-					options: {},
-				},
-			},
-			{
-				test: /\.js$/,
-				loader: ['babel-loader'],
-				exclude: /node_modules/,
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'src/index.html',
-		}),
-		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin({
-			filename: '[name]-[contenthash:10].css',
-			publicPath: '../',
-		}),
-	],
+  mode: 'production',
+  entry: './src/js/main.js',
+  output: {
+    path: path.resolve(__dirname, '../', 'dist'),
+    filename: '[name]-[contenthash:10].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets',
+              publicPath: 'assets',
+            },
+          },
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {},
+        },
+      },
+      {
+        test: /\.js$/,
+        loader: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name]-[contenthash:10].css',
+      publicPath: '../',
+    }),
+    new CopyPlugin([{ from: 'src/assets', to: 'assets' }]),
+  ],
 };
