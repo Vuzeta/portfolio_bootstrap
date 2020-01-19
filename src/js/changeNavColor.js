882fd4e79507182b0024ba328d8cd5a4b2a2374b
Vuzeta <34634478+Vuzeta@users.noreply.github.com>
2020-01-19 20:09:18 +0100

    fixed changeNavColor, description about me

--- src/js/changeNavColor.js
+++ src/js/changeNavColor.js
@@ -1,13 +1,24 @@
 const changeColor = () => {
   let fromTop = window.pageYOffset;
   const navigation = document.querySelector('.navigation');
-  console.log(window.pageYOffset);
+  const navigationMenu = document.querySelector('.navigation__menu');
+  const navigationItem = document.querySelectorAll('.navigation__item');
   if (fromTop >= 60) {
     navigation.style.backgroundColor = '#f2f2f2';
     navigation.style.boxShadow = '2px 2px 2px rgba(0, 0, 0, 0.1)';
+    navigationMenu.style.backgroundColor = '#f2f2f2';
+    navigationItem.forEach(item => {
+      item.style.backgroundColor = '#f2f2f2';
+    });
   } else {
     navigation.style.backgroundColor = '#fff';
     navigation.style.boxShadow = '0px 0px 0px rgba(0, 0, 0, 0.1)';
+    navigationMenu.style.backgroundColor = '#fff';
+    navigationMenu.style.boxShadow = '0px 0px 0px rgba(0, 0, 0, 0.1)';
+    navigationItem.forEach(item => {
+      item.style.backgroundColor = '#fff';
+      item.style.boxShadow = '0px 0px 0px rgba(0, 0, 0, 0.1)';
+    });
   }
 };
 
