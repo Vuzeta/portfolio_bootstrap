80683fa00e12ea80e980b4265b529277041fd780
Vuzeta <machniik95@gmail.com>
2020-01-20 12:15:25 +0100

    add animateHeader.js

---
+++ src/js/animateHeader.js
@@ -0,0 +1,14 @@
+const animateHeader = () => {
+	var element = document.querySelector('.hero__header');
+	var photo = document.querySelector('.hero__photo');
+	var position = element.getBoundingClientRect();
+	if (position.top > 0) {
+		element.style.transform = 'scale(1)';
+		photo.style.transform = 'scale(1)';
+	} else if (position.top < -500) {
+		element.style.transform = 'scale(0)';
+		photo.style.transform = 'scale(0)';
+	}
+};
+
+export default animateHeader;
--- src/js/main.js
+++ src/js/main.js
@@ -3,23 +3,26 @@ import { toggleHamburger, linkClicked } from './hamburger';
 import initProjects from './projectsList';
 import initTech from './technologiesList';
 import changeColor from './changeNavColor';
+import animateHeader from './animateHeader';
 
 const hamburger = document.querySelector('.hamburger');
 const links = document.querySelectorAll('.navigation__link');
 
 window.addEventListener('DOMContentLoaded', () => {
-  initProjects();
-  initTech();
+	initProjects();
+	initTech();
+	animateHeader();
 });
 window.addEventListener('scroll', changeColor);
+window.addEventListener('scroll', animateHeader);
 
 hamburger.addEventListener('click', function() {
-  toggleHamburger(this);
+	toggleHamburger(this);
 });
 
 links.forEach(link => link.addEventListener('click', e => linkClicked(e)));
 
 function importAll(r) {
-  r.keys().forEach(r);
+	r.keys().forEach(r);
 }
 importAll(require.context('../assets/', true, /\.(png|jpe?g|svg)$/));
